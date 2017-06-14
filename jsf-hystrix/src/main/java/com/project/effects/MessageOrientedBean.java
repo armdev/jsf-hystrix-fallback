e8ee72ad5f6787fc3ebcaa8cfea2a95db61d4a64
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-06-14 15:55:23 +0400

    Bix Bank

--- jsf-hystrix/src/main/java/com/project/effects/MessageOrientedBean.java
+++ jsf-hystrix/src/main/java/com/project/effects/MessageOrientedBean.java
@@ -1,9 +1,11 @@
 package com.project.effects;
 
 import java.util.Random;
+import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,9 +15,11 @@ import lombok.Setter;
  *
  * @author armdev
  */
-@ManagedBean
+
 @AllArgsConstructor
 @NoArgsConstructor
+@Named
+@RequestScoped
 public class MessageOrientedBean {
 
     private final String[] effects = {"blind", "clip", "drop", "explode", "fold", "puff", "slide", "scale", "bounce", "pulsate", "shake", "size"};
